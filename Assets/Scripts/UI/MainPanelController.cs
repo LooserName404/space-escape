e71c905bfe892ff3dcb704e5412760185a99c88e
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2021-09-25 13:40:51 -0300

    feat: add main menu sample

---
+++ Assets/Scripts/UI/MainPanelController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace SpaceEscape.UI
+{
+    public class MainPanelController : MonoBehaviour
+    {
+        public void OnPlayButtonPressed()
+        {
+            SceneManager.LoadScene("PlayScene");
+        }
+
+        public void OnExitButtonPressed()
+        {
+#if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
